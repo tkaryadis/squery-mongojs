90d84c4f3f6eafd81b2e80fa4fddc65b185dd4bb
tkaryadis <tkaryadis@gmail.com>
2021-11-15 20:20:14 +0200

    add callback-promise fns

--- src/cmql_js/util.cljs
+++ src/cmql_js/util.cljs
@@ -2,6 +2,8 @@
   (:require-macros [cmql-js.util])
   (:require [cljs.core.async :refer [go  <! take!]]))
 
-(defn run-query
-  ([f cb] (take! (f) (fn [r] (cb r))))
-  ([f] (js/Promise. (fn [resolve _] (go (resolve (<! (f))))))))
+(defn js-cb [f cb]
+  (take! (f) (fn [r] (cb r))))
+
+(defn js-p [f]
+  (js/Promise. (fn [resolve _] (go (resolve (<! (f)))))))
