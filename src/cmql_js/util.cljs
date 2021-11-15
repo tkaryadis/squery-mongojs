d9148a5ecd60c17055fe2148b0ebf46735481b58
tkaryadis <tkaryadis@gmail.com>
2021-11-15 20:31:30 +0200

    add callback-promise fns

--- src/cmql_js/util.cljs
+++ src/cmql_js/util.cljs
@@ -2,8 +2,6 @@
   (:require-macros [cmql-js.util])
   (:require [cljs.core.async :refer [go  <! take!]]))
 
-(defn js-cb [f cb]
-  (take! (f) (fn [r] (cb r))))
-
-(defn js-p [f]
-  (js/Promise. (fn [resolve _] (go (resolve (<! (f)))))))
+(defn js-async
+  ([f cb] (take! (f) (fn [r] (cb r))))
+  ([f] (js/Promise. (fn [resolve _] (go (resolve (<! (f))))))))
